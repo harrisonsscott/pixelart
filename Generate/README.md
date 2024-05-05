1e08a459b55d84987d8701dcca373bb841e83616
harrisonsscott <harrisonsterlingscott@gmail.com>
2024-05-05 11:25:40 -0700

    Added palettes

--- Generate/README.md
+++ Generate/README.md
@@ -4,3 +4,11 @@ OpenCV is required for the script to run, install with
 sudo apt install libopencv-dev
 
 VSCode will probably think that the OpenCV includes aren't valid but it will compile just fine.
+
+The way the script is it resizes an image, loops over every pixel, puts the similar colors in a palette, and then writes all the pixels with
+the corresponding palette color to json.
+
+Run with
+/pixelArt [Image] [Size X] [Size Y] [Threshold]
+
+Where Size X and Y is the size of the outputted image, and threshold is the similarity between palette colors
--- Generate/main.cpp
+++ Generate/main.cpp
@@ -5,9 +5,16 @@
 using namespace cv;
 using namespace std;
 
+float getSimilarity(Vec3b color1, Vec3b color2){
+	return
+		0.11 * std::pow(color1[0] - color2[0], 2) +
+		0.59 * std::pow(color1[1] - color2[1], 2) +
+		0.3 * std::pow(color1[2] - color2[2], 2);
+}
+
 int main(int argc, char *argv[]){
     if (argc < 4){
-        cout << "./pixelArt [Image] [Size X] [Size Y]" << endl;
+        cout << "./pixelArt [Image] [Size X] [Size Y] [Threshold]" << endl;
         return -1;
     }
 
@@ -23,7 +30,41 @@ int main(int argc, char *argv[]){
         return -1;
     }
 
-    cout << size << endl;
-        
+    Mat im;
+    vector<Vec3b> palette; 
+
+    resize(image, im, size);
+
+    for (int x = 0; x < size.width; x++){
+        for (int y = 0; y < size.height; y++){
+            int similarity = 10000000;
+            int index = 0;
+
+            Vec3b& color = im.at<Vec3b>(Point(y, x));
+            Vec3b selectedColor = color;
+
+            for (int i = 0; i < palette.size(); i++){
+                int similarity2 = getSimilarity(color, palette[i]);
+                if (similarity2 < similarity){
+                    similarity = similarity2;
+                    selectedColor = palette[i];
+                    index = i;
+                }
+            }
+
+            if (similarity > 100){
+                palette.push_back(color);
+            }
+
+            im.at<Vec3b>(Point(y, x)) = selectedColor;
+        }
+    }
+
+
+    imwrite("out.png", im);
+
+    image.release();
+    im.release();
+
     return 0;
 }
