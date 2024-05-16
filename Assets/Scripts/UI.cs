355c1b00a32d539f3fce3b1a73e0aa75486600f4
harrisonsscott <harrisonsterlingscott@gmail.com>
2024-05-16 12:37:58 -0700

    added a PlaceColor function

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,16 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UI : MonoBehaviour
 {
+    // in the bottom of the ui, a long list of all the colors are laid out and are able to be selected
+    public GameObject colorReference; // make sure this has the RawImage, and Button components and a child with TMP_TEXT
+    public Transform bottom; // bottom of the UI where you can select the colors
+    public Transform colorContent; // where all the colorReference clones are placed
+
     void Start()
     {
-        
+        PlaceColor("#ff7700");
+        PlaceColor("#ff00ff");
     }
 
-    void Update()
-    {
-        
+    public void PlaceColor(string hex){ // clones colorReference and places it in colorContent
+        Color color = hex.ToRGB();
+        GameObject clone = Instantiate(colorReference, colorContent);
+        clone.GetComponent<RawImage>().color = color;
+        clone.transform.GetChild(0).GetComponent<TMP_Text>().text = colorContent.childCount + "";
     }
 }
