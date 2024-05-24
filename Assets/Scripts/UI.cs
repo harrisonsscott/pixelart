3762a29ed8e11afdffdbdcfa8b7e251e7f00ed86
harrisonsscott <harrisonsterlingscott@gmail.com>
2024-05-23 17:56:45 -0700

    Added a ChangeColor function

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
 
 public class UI : MonoBehaviour
 {
     // in the bottom of the ui, a long list of all the colors are laid out and are able to be selected
     public GameObject colorReference; // make sure this has the RawImage, and Button components and a child with TMP_TEXT
+    public GameObject colorIndicator; // thin strip that indicates the current color
     public Transform bottom; // bottom of the UI where you can select the colors
     public Transform colorContent; // where all the colorReference clones are placed
 
     void Awake()
     {
         ClearColors();
+    }
+
+    public void ChangeColor(Color color){
+        colorIndicator.GetComponent<RawImage>().color = color;
+    }
+
+    public void ChangeColor(string hex){
+        ChangeColor(hex.ToRGB());
+    }
+
+    public void ChangeColor(Vector4 color){
+        ChangeColor(color.ToColor());
     }
 
     public void ClearColors(){ // removes all the children in colorContent
