c507720572e01d2134d51d2b0a27e61c1aca3998
harrisonsscott <harrisonsterlingscott@gmail.com>
2024-05-25 17:37:08 -0700

    Added a progress amount

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class ProgressBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Vector2 size;
+    private RectTransform amount; // rect transform of the child
+    private float progress; // from 0 to 1
+    public float Progress {
+        get {
+            return progress;
+        }
+        set {
+            Debug.Log(size.x);
+            progress = Mathf.Clamp(value, 0, 1);
+            amount.offsetMax = new Vector2(-progress*size.x, 0);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        amount = transform.GetChild(0).GetComponent<RectTransform>();
+        size = gameObject.GetComponent<RectTransform>().sizeDelta;
+
+        Progress = 0.5f;
     }
 }
