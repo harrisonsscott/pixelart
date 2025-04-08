cba5698dd28982cce852a2aea07a4aa508112968
harrisonsscott <harrisonsterlingscott@gmail.com>
2025-04-07 22:27:19 -0700

    Added a decoder file

---
+++ Generate/decode.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include "json.hpp"
+
+using namespace cv;
+using namespace std;
+using json = nlohmann::json;
+
+int main(int argc, char *argv[]){
+    return 0;
+}
