1d1e48af4004385c0be6b87c0152c3772f3c07a5
harrisonsscott <harrisonsterlingscott@gmail.com>
2025-04-07 23:17:34 -0700

    Added error catching for the image size

--- Generate/main.cpp
+++ Generate/main.cpp
@@ -10,17 +10,57 @@ using namespace std;
 using json = nlohmann::json;
 
 int main(int argc, char *argv[]){
-    cv::Mat image = cv::imread(argv[1], cv::IMREAD_UNCHANGED);
-    cout << image.channels() << endl;
-    if (image.channels() == 3){
-        cvtColor(image, image, COLOR_BGR2BGRA);
+    const Size maxSize(256, 256); // the maximum size an output image can be (recommended to be 256x256)
+
+    if (argc < 5){
+        cout << "./pixelArt [Image] [Output] [Size X] [Size Y] [Threshold]" << endl;
+        return -1;
     }
+
+    // Load the image with 3 or 4 color channels
+    cv::Mat image = cv::imread(argv[1], cv::IMREAD_UNCHANGED);
+    if (image.empty()){
+        cout << "Error loading image " << argv[1] << endl;
+        return -1;
+    }
+    
+    // if (image.channels() == 3){
+    //     cvtColor(image, image, COLOR_BGR2BGRA);
+    // }
+
+    Size size = Size(atoi(argv[3]), atoi(argv[4]));
+
+    if (size.area() <= 0){
+        cout << "Invalid output size! " << size << endl;
+        return -1;
+    }
+
+    // images need to be square
+    int sideLength = max(size.width, size.height);
+    
+    // equal to 256*256
+    if (sideLength*sideLength > maxSize.area()){
+        cout << "Image is too large, max size is " << maxSize << endl;
+        return -1;
+    }
+
+    if (image.size().area() < sideLength * sideLength){
+        cout << "Output size is large than the input size!" << endl;
+        return -1;
+    }
+    
+    if (sideLength*sideLength != size.area()){
+        cout << "Image isn't square, converting to " << Size(sideLength, sideLength) << endl;
+        size = Size(sideLength, sideLength);
+    }
+
+
 
     for (int y = 0; y < 8; y++){
         for (int x = 0; x < 8; x++){
             Vec3b color = image.at<Vec3b>(Point(x, y));
             Vec4b color2 = image.at<Vec4b>(Point(x, y));
-            cout << color << " - " << color2 << endl;
+            // cout << color << " - " << color2 << endl;
         }  
     }
 
